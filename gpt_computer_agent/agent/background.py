3b2b8f53651b020213e51da13a849a8c72d5ba44
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-05 02:17:50 +0600

    Update background.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- gpt_computer_agent/agent/background.py
+++ gpt_computer_agent/agent/background.py
@@ -4,11 +4,6 @@ from .chat_history import *
 
 llm_history_oiginal = [
     SystemMessage(
-        content=[
-            {"type": "text", "text": "You are an helpful and intelligent agent. But converting your text to the speech process can be long so please make short your answers as possible."},
-            {"type": "text", "text": "Answer with maximum 3 sentences. Also please feel free to use tools."},
-            {"type": "text", "text": "If you want to make a long answer using clipboard tool. And say i just copied the answer. Use this way for codes, text fixes."},
-            {"type": "text", "text": "If the user wantt to take a action just make the action and say ok. Like copy to clipboard."}
-                 ]
+        content="You are a helpful and intelligent agent. But converting your text to the speech process can be long so please make your answers as short as possible. Answer with a maximum of 3 sentences. Also, please feel free to use tools. If you want to make a long answer, use the clipboard tool and say 'I just copied the answer.' Use this method for codes, text fixes. If the user wants to take an action, just make the action and say 'ok.' Like copying to clipboard."
                  )
     ]
