f4e8938a4bb1d58d30f6cc3bb6809280c4072637
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-27 02:43:19 +0600

    Update setup.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ elif platform.system() == "Darwin": # Darwin is the system name for macOS
 
 setup(
     name="gpt_computer_agent",
-    version="0.19.2",
+    version="0.19.1",
     description="""GPT""",
     long_description="".join(open("README.md", encoding="utf-8").readlines()),
     long_description_content_type="text/markdown",
