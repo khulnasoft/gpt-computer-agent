32358b323755e21bb4f4fdfcbc44f439f9ede021
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-07-01 16:30:39 +0600

    Update llm.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- gpt_computer_agent/llm.py
+++ gpt_computer_agent/llm.py
@@ -6,8 +6,16 @@ from langchain_groq import ChatGroq
 
 try:
     from .utils.db import load_api_key, load_openai_url, load_model_settings, load_groq_api_key, load_google_api_key
+    from .custom_callback import customcallback
 except ImportError:
     from utils.db import load_api_key, load_openai_url, load_model_settings, load_groq_api_key, load_google_api_key
+    from custom_callback import customcallback
+
+
+
+the_callback = customcallback(strip_tokens=False, answer_prefix_tokens=["Answer"])
+
+
 
 def get_model(high_context=False):
     the_model = load_model_settings()
@@ -15,16 +23,16 @@ def get_model(high_context=False):
     the_groq_api_key = load_groq_api_key()
     the_google_api_key = load_google_api_key()
     the_openai_url = load_openai_url()
-    
+
     def open_ai_base(high_context):
         if the_openai_url == "default":
             true_model = the_model
             if high_context:
                 true_model = "gpt-4-turbo"
-            return {"model": true_model, "api_key": the_api_key, "max_retries":15}
+            return {"model": true_model, "api_key": the_api_key, "max_retries":15, "streaming":True, "callbacks":[the_callback]}
         else:
-            return {"model": the_model, "api_key": the_api_key, "max_retries":15, "base_url": the_openai_url}
-    
+            return {"model": the_model, "api_key": the_api_key, "max_retries":15, "streaming":True, "callbacks":[the_callback], "base_url": the_openai_url}
+
     args_mapping = {
         ChatOpenAI: open_ai_base(high_context=high_context),
         ChatOllama: {"model": the_model},
@@ -37,7 +45,7 @@ def get_model(high_context=False):
         "gpt-4-turbo": (ChatOpenAI, args_mapping[ChatOpenAI]),
         "gpt-3.5": (ChatOpenAI, args_mapping[ChatOpenAI]),
         "gpt-3.5-turbo": (ChatOpenAI, args_mapping[ChatOpenAI]),
-        
+
         # Google Generative AI - Llama
         "llava": (ChatOllama, args_mapping[ChatOllama]),
         "llama3": (ChatOllama, args_mapping[ChatOllama]),
@@ -45,11 +53,11 @@ def get_model(high_context=False):
 
         # Google Generative AI - Gemini
         "gemini-pro": (ChatGoogleGenerativeAI, args_mapping[ChatGoogleGenerativeAI]),
-        
+
         # Groq
         "mixtral-8x7b-groq": (ChatGroq, args_mapping[ChatGroq])
     }
-    
+
     model_class, args = model_mapping[the_model]
     return model_class(**args) if model_class else None
 
